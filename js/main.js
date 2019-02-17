d2c033a2720b4cfd32d3ac40538d3534531f942b
Uche Jude <iamuchejude@gmail.com>
2019-02-17 02:00:56 +0100

    update, upgrades and ++progress

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-const setActiveContent = content => {
-  document.querySelectorAll('main > div').forEach((contents) => contents.classList.remove('active'));
-  document.querySelector(`main > div[data-content="${content}"]`).classList.add('active');
+const prepareActivies = () => {
+  if (window.innerWidth <= '800') void(document.querySelector('#menu').classList.remove('mobile-active'));
+  return void document.querySelectorAll('div[data-content]').forEach(x => x.classList.remove('active'));
 }
 
 document.querySelector('#logo a').addEventListener('click', (e) => {
   e.preventDefault();
-  setActiveContent('home');
+  prepareActivies();
+  return void document.querySelector('.body').scrollIntoView({ behavior: 'smooth' });
 });
+
+const makeContentActive = (content) => {
+  prepareActivies();
+  content.scrollIntoView({ behavior: 'smooth' });
+  return void content.classList.add('active');
+}
 
 document.querySelectorAll('#menu ul li a:not([data-action="external"])').forEach((menu) => {
   menu.addEventListener('click', (e) => {
     e.preventDefault();
-    // setActiveContent(e.target.getAttribute('data-show'));
+    const target = e.target.getAttribute('data-show');
+    return void makeContentActive(document.querySelector(`[data-content="${target}"]`));
   });
 });
 
 document.querySelector('#mobile .icon').addEventListener('click', e => {
   e.preventDefault();
-  document.querySelector('#menu').classList.toggle('mobile-active');
+  return void document.querySelector('#menu').classList.toggle('mobile-active');
 });
+
+document.querySelector('#year').textContent = (new Date()).getFullYear();
